734b7eeadc5609f8505581d76d9f8853ad60a31f
tamal15 <68095422+tamal15@users.noreply.github.com>
2021-06-11 23:24:16 +0600

    Add files via upload

--- ecommarce/urls.py
+++ ecommarce/urls.py
@@ -4,11 +4,12 @@ from django.urls import path, include
 
 from django.views.static import serve
 from django.conf.urls import url
+from ecommarce import settings
  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+    path('', include('shop.urls')),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-
+  
 ]
